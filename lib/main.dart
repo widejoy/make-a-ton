89b405745608995eed44ed197d0195bb674dc7b2
revertionist <govindrnair.rev@gmail.com>
2023-11-04 19:30:10 +0530

    resolve

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:my_project/screens/create_post_screen.dart';
+import 'package:my_project/screens/featured.dart';
+import 'package:my_project/screens/near_me.dart';
+import 'package:my_project/widgets/drawer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -7,25 +11,58 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(),
-      body: const MyHomePage(),
-    ));
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
+class _MyAppState extends State<MyApp> {
+  int currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 220, 101, 4),
+        ),
+        drawer: const MyDrawer(),
+        bottomNavigationBar: NavigationBar(
+          backgroundColor: const Color.fromARGB(255, 220, 101, 4),
+          indicatorColor: Colors.amber[800],
+          selectedIndex: currentPageIndex,
+          destinations: const <Widget>[
+            NavigationDestination(
+              selectedIcon: Icon(Icons.area_chart),
+              tooltip: 'Click here to find posts near you',
+              icon: Icon(Icons.area_chart_outlined),
+              label: 'Near Me',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.featured_play_list),
+              icon: Icon(Icons.featured_play_list_outlined),
+              tooltip: 'Click here to find the hottest posts',
+              label: 'Featured',
+            ),
+            NavigationDestination(
+                tooltip: 'Click here to upload an issue',
+                selectedIcon: Icon(Icons.create_rounded),
+                icon: Icon(Icons.create_outlined),
+                label: 'Report a problem')
+          ],
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+        ),
+        body: <Widget>[
+          const NearMe(),
+          const Featured(),
+          const CreatePostScreen()
+        ][currentPageIndex],
+      ),
+    );
   }
 }
