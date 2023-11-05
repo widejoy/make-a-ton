fc0e648611d99e9cad90463a2b63e3e273ea9c88
revertionist <govindrnair.rev@gmail.com>
2023-11-05 07:05:44 +0530

    Dummy data for essential pages

---
+++ lib/screens/trainTimings.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class Train extends StatefulWidget {
+  const Train({super.key});
+
+  @override
+  State<Train> createState() => _TrainState();
+}
+
+class _TrainState extends State<Train> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        navigationBarTheme: const NavigationBarThemeData(
+          labelTextStyle: MaterialStatePropertyAll(
+            TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 220, 101, 4),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+        body: Column(
+          children: [
+            Image.asset(
+              'assets/train.jpeg',
+              height: 300,
+              alignment: Alignment.topCenter,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Train Timings",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w300),
+            ),
+            const SizedBox(height: 20),
+            const ListTile(
+              title: Text("Train 1",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300)),
+              subtitle: Text("Time -> 12:00pm"),
+            ),
+            const ListTile(
+              title: Text("Train 2",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300)),
+              subtitle: Text("Time -> 12:00pm"),
+            ),
+            const ListTile(
+              title: Text("Train 3",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300)),
+              subtitle: Text("Time -> 12:00pm"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
